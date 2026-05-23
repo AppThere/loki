406b779e079e3d7c1bae92cf5d7771d54083dbd3
Kevin Carlson <kdesltd@gmail.com>
2026-05-23 13:37:32 -0400

    Fixed Android entry point for Dioxus apps

--- loki-spreadsheet/src/main.rs
+++ loki-spreadsheet/src/main.rs
@@ -6,15 +6,13 @@
 //! Launches the Dioxus Native application. All application logic lives in the
 //! `loki_spreadsheet` library crate (`src/lib.rs`).
 
-#[cfg(not(target_os = "android"))]
 fn main() {
     loki_i18n::init();
     dioxus::launch(loki_spreadsheet::app::App);
 }
 
 #[cfg(target_os = "android")]
-#[android_activity::main]
-fn android_main(_app: android_activity::AndroidApp) {
-    loki_i18n::init();
-    dioxus::launch(loki_spreadsheet::app::App);
+#[no_mangle]
+pub extern "C" fn android_main(_app: *mut std::ffi::c_void) {
+    main();
 }
